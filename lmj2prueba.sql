ad6e06bb6dde2015535a7bf1b0bf3fcd17539865
equipoLmj2 <equipolmj2@gmail.com>
2021-11-26 07:08:10 -0500

    se ingresa base de datos

--- lmj2prueba.sql
+++ lmj2prueba.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-11-2021 a las 19:59:57
+-- Tiempo de generación: 26-11-2021 a las 13:06:16
 -- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.11
+-- Versión de PHP: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `lmj2`
+-- Base de datos: `lmj2prueba`
 --
 
 -- --------------------------------------------------------
@@ -31,10 +31,17 @@ CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
   `placa_vehiculo` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `ingreso` datetime NOT NULL,
-  `salida` datetime NOT NULL,
-  `estadia` time NOT NULL,
-  `precio` int(11) NOT NULL
+  `salida` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `placa_vehiculo`, `ingreso`, `salida`) VALUES
+(1, 'rpg003', '2021-11-25 10:57:08', '2021-11-25 23:55:50'),
+(2, 'rpg005', '2021-11-25 11:28:33', NULL),
+(3, 'cosi000', '2021-11-25 23:24:52', '2021-11-25 23:54:17');
 
 -- --------------------------------------------------------
 
@@ -49,6 +56,18 @@ CREATE TABLE `usuarios` (
   `movil` varchar(10) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`cedula`, `nombres`, `fijo`, `movil`) VALUES
+(138, 'john', '13856', '31566315'),
+(3185, 'Leo', '1386', '318315'),
+(13861, 'Marco', '3518', '135845'),
+(13865, 'juan', '31851', '35185'),
+(16815, 'cosi', '1615', '316648351'),
+(3581135, 'Marco', '355451', '531385');
+
 -- --------------------------------------------------------
 
 --
@@ -60,6 +79,18 @@ CREATE TABLE `vehiculos` (
   `marca` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
   `cc_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `vehiculos`
+--
+
+INSERT INTO `vehiculos` (`placa`, `marca`, `cc_usuario`) VALUES
+('cosi000', 'bmw', 16815),
+('rpg001', 'bmw', 138),
+('rpg002', 'ferrari', 13865),
+('rpg003', 'mercedes benz', 3185),
+('rpg004', 'lamborgini', 13861),
+('rpg005', 'lotus', 3581135);
 
 --
 -- Índices para tablas volcadas
@@ -93,7 +124,7 @@ ALTER TABLE `vehiculos`
 -- AUTO_INCREMENT de la tabla `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
